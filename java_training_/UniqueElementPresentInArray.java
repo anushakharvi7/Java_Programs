98a8d7034a1e58a5f6f604d06d3491d120bf3794
Anusha <anushakharvi015@gmail.com>
2026-07-13 14:27:03 +0530

    add files

--- java_training_/UniqueElementPresentInArray.java
+++ java_training_/UniqueElementPresentInArray.java
@@ -1,10 +1,34 @@
 package java_training_;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class UniqueElementPresentInArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of array ");
+		int size=sc.nextInt();
+		int[] arr=new int[size];
+		for (int i=0;i<=arr.length-1;i++)
+		{
+			System.out.println("Enter the element for the index:"+i);
+			arr[i]=sc.nextInt();
+		}
+		System.out.println(Arrays.toString(arr));
+		for(int i=0;i<=arr.length-1;i++) {
+			int count=0;
+			int val=arr[i];
+			for(int j=0;j<=arr.length-1;j++) {
+				if(val==arr[j])
+					count++;
+			}
+			if(count==1)
+			
+				System.out.println(val);
+			
+		}
+		}
 	}
 
-}
+
